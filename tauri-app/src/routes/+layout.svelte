0ef1d64c8791db9123842680a2bf10dfb6e92103
DevKeravi <98294906+sungryeolchae@users.noreply.github.com>
2023-01-08 20:51:08 +0900

    project initialized and tailwind set

---
+++ tauri-app/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { base } from "$app/paths";
+  import { component_subscribe } from "svelte/internal";
+
+  // import "../app.css";
+  import "../reset.css"
+</script>
+
+<main class="container">
+  <slot />
+  <style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+  </style>
+</main>
